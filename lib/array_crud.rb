235c21c8e4886be104120b88621fefe0a40c638e
Nicholas Strawn <nicholasjstrawn@gmail.com>
2021-04-21 20:31:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = ["John", "Paul", "George"]
-  element = "Ringo"
-  my_array << element
+  my_array = ["wow", "I", "am", "really", "learning"]
+  my_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
